ef939d72004608168a23f1dd39fa1fe4f48316ae
Raghu_H <raghu.rohan95@gmail.com>
2025-09-16 14:58:04 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,9 +1,22 @@
+provider "aws" {
+  region = "us-east-1" # Change to your preferred region
+}
+
 terraform {
+  required_version = ">= 1.11.0"
   backend "s3" {
     bucket        = "897722687643-web-tf-state-bucket"
     key           = "usecase7/terraform.tfstate"
     region        = "us-east-1"
     encrypt       = true
     use_lockfile  = true # Enables native S3 state locking
+  }
+}
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
   }
 }
